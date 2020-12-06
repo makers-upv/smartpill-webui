6e3bf9515ff3b18a5e2cdc6ba92c0e09aaaf3d0e
Jaime Laborda <jaimelaborda@gmail.com>
2020-12-06 19:26:37 +0100

    Now we have a working clock on status page

--- src/app/page-status/page-status.page.ts
+++ src/app/page-status/page-status.page.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-status.page.scss'],
 })
 export class PageStatusPage implements OnInit {
+  time = new Date();
+  timer;
 
   constructor() { }
 
   ngOnInit() {
+    this.timer = setInterval(() => {
+      this.time = new Date();
+      console.log(this.time);
+    }, 1000);
   }
 
 }
